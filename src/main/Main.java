dd78abbc307b8625b0b3b4453abd55efcaa12c8b
Calin Carcan <calincarcan@gmail.com>
2022-11-08 22:31:54 +0200

    Start

--- src/main/Main.java
+++ src/main/Main.java
@@ -70,7 +70,7 @@ public final class Main {
         ArrayNode output = objectMapper.createArrayNode();
 
         //TODO add here the entry point to your implementation
-        System.out.println("Calin");
+
 
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
         objectWriter.writeValue(new File(filePath2), output);
